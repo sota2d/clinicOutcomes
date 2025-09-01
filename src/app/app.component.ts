82beda9fc86375f9ead29cf8c49cd46b0a9b3ab3
sota2d <sota2d@me.com>
2025-08-31 20:26:30 -0700

    Added Radio Button Group with test functionality and some initial styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FilterButtonGroupComponent } from './features/filter-button-group/filter-button-group.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FilterButtonGroupComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
